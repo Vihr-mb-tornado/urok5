b1624593e6ed3f71fab1447e6ab2b8076d0ca875
Laptop <yevhen003@gmail.com>
2024-10-06 10:56:24 +0200

    Initial commit

---
+++ character.py
@@ -0,0 +1,40 @@
+﻿import random
+
+class Character:
+    name = ''
+    health = 100
+    damage = 1
+    defence = 0
+    damage_offset = 0.2
+
+    def __init__(self, name, health, damage, defence):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
+
+    def show_stats(self):
+        print(self)
+
+    def __str__(self):
+        return f' -< {self.name} >-\n' \
+               f' Здоров`я: {self.health}\n'\
+               f' Шкода: {self.damage}\n'\
+               f' Захист: {self.defence}\n'
+
+    def count_incoming_damage(self, damage):
+        return round(max(damage * ((100 - self.defence) / 100), 0), 2)
+
+    def count_damage_offset(self):
+        return random.randint(0, int(self.damage * self.damage_offset)) * random.choice((-1, 1))
+
+    def is_alive(self):
+        return self.health > 0
+
+    def take_damage(self, damage):
+        real_damage = self.count_incoming_damage(damage)
+        self.health = round(max(self.health - real_damage, 0), 2)
+        return real_damage
+
+    def attack(self, target):
+        return target.take_damage(self.damage + self.count_damage_offset())
---
+++ main2.py
@@ -0,0 +1,28 @@
+﻿from character import Character
+from berserk import Berserk
+
+def attack_message(attacker: Character, target: Character, damage_done: float) :
+    return f'{attacker.name} атакував {target.name}.\n' \
+           f'{attacker.name} наніс {damage_done} шкоди. ' \
+           f'У {target.name} залишилося {target.health} здоров`я.'
+
+
+player1 = Character('Vasya', 100, 25, 50)
+player2 = Berserk('Petya.Berserk', 100, 10, 12)
+
+print(f'Створено нового персонажа: {player1.name}')
+print(f'Створено нового персонажа: {player2.name}')
+
+player1.show_stats()
+player2.show_stats()
+count = 0
+while player1.is_alive() and player2.is_alive():
+    count = count + 1
+    damage_done = player1.attack(player2)
+    print(attack_message(player1, player2, damage_done))
+
+    damage_done = player2.attack(player1)
+    print(attack_message(player2, player1, damage_done))
+
+print(f'Обмін ударів :{count}')
+print(f'{player1}\n{player2}')
